61e4b5d3a06cd7c664e54e3b26785db1edbeceea
hf-ikea <forma3425@gmail.com>
2025-07-29 21:17:43 -0700

    hmm

---
+++ hamlib-sys/src/native/wrapper.h
@@ -0,0 +1,10 @@
+//#include "ampclass.h"  c++ bindings
+#include "amplifier.h"
+#include "amplist.h"
+#include "multicast.h"
+#include "rig.h"
+//#include "rigclass.h"  c++ bindings
+#include "riglist.h"
+#include "rotator.h"
+//#include "rotclass.h"  c++ bindings
+#include "rotlist.h"
